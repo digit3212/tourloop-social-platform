f2905af44b655d1a1502ed72910b893a932653e6
digit3212 <digit3212@gmail.com>
2025-11-27 15:59:48 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return (
+    <div class="text-center p-10 text-3xl text-fb-blue font-bold">
+      تم التشغيل بنجاح ✔
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
